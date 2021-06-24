4bda9390266639e3524c76974139953839aecba7
Sleep <git.sleep@gmail.com>
2021-06-24 16:46:06 +0100

    Add files via upload

---
+++ learningPython/python/1helloWorld.py
@@ -0,0 +1,16 @@
+
+    # print ("Hello, Sir.")
+    # print ("It is good to see you")
+
+T = ("Sleep")
+L = ("Miss Eady")
+EV = ("\"DE-V17.0\"")
+
+print ("\n\n\n\n\n\n\n")
+
+print ("Hello " + T + ", it is good to see you! :)")
+print ("My name is " + EV + ", but call me 'EV' ;)" + "\n\n\n\n\n\n\n" )
+
+    # print ("Good afternoon, " + L + " :)")
+    # print ("My name is " + EV + ", but call me 'EV' ;)" + "\n\n\n\n\n\n\n" )
+
---
+++ learningPython/python/2arithmetic.py
@@ -0,0 +1,19 @@
+
+product = 7 * 7
+print (product)
+
+remainder = 1398 % 11
+print (remainder)
+
+one = 1234567891011121314151617181920 * 1234567891011121314151617181920
+print (one)
+
+two1 = 5
+two2 = 7
+two1 -= two2
+print (two2)
+
+three1 = 7.5
+three2 = 5
+three3 = three1 * three2
+print (three3)
