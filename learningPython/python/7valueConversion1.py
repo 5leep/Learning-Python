640b6c0a4e962882858293f77d54b072527aa619
Sleep <git.sleep@gmail.com>
2021-07-12 14:58:17 +0100

    Add files via upload

---
+++ learningPython/python/7valueConversion1.py
@@ -0,0 +1,11 @@
+age = 21
+print ("I am " + str(age) + " years old!")
+
+A = "10"
+B = "20"
+string_addition = A + B
+print (string_addition)
+
+int_addition = int(A) + int(B)
+print (int_addition)
+
---
+++ learningPython/python/7valueConversion2.py
@@ -0,0 +1,10 @@
+float_1 = 7.5
+float_2 = 3.5
+
+product = float_1 * float_2
+
+print(product)
+
+big_string = float_1 * float_2
+
+print ("The product was", big_string) 
