81c9825ae47b81b983280bf1d313cc50ee1d0428
Sleep <git.sleep@gmail.com>
2021-07-30 23:11:34 +0100

    Add files via upload

---
+++ 4division_1.1.py
@@ -0,0 +1,11 @@
+# The following syntax provides the end result as it's raw number...
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers / num_people
+print (whole_cucumbers_per_person)
+
+# ... while this syntax provides the end result as a whole number.
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers / num_people
+print (int(whole_cucumbers_per_person))
---
+++ 6booleans_1.1.py
@@ -0,0 +1,11 @@
+
+number = "2"
+
+print ("\n\n\nHello sir,\n")
+
+if number == "1":
+    print ("   " + number + "\n   ^\n" + "This number is 1.\n\n\n")
+elif number != "1":
+    print ("   " + number + "\n   ^\n" + "This number is not 1.\n\n\n")
+else:
+    print ("heehoo")
