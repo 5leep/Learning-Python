9720f2ad31d81a87d220fb41316cf3a65568f629
Sleep <git.sleep@gmail.com>
2021-09-02 19:25:00 +0100

    Add files via upload

---
+++ 10variables.py
@@ -0,0 +1,19 @@
+meal = "Granola n' beans"
+
+print ("\n\n\nBreakfast:")
+print (meal)
+
+meal = "Milk with beef"
+
+print ("\nLunch:")
+print (meal)
+
+meal = "baked shelf with cola sriracha glaze"
+
+print ("\nDinner:")
+print (meal)
+
+meal = "medium rare soup with crayons"
+
+print ("\nDessert:")
+print (meal + "\n\n\n")
---
+++ 11errors.py
@@ -0,0 +1,7 @@
+# print("this will not print. Reason: mismatched quotation marks.')
+# print(unprintable)
+
+# Should Be...
+
+print("this will print")
+print("printable")
