6f777580cfc009ca23e28d38a6eb4d8de6da5cb1
Sleep <git.sleep@gmail.com>
2021-09-03 14:39:32 +0100

    Add files via upload

---
+++ learningPython/python/12numbers.py
@@ -0,0 +1,15 @@
+an_int  = 2
+a_float = 2.1
+
+print(an_int + 3)
+
+
+# Define the release and runtime integer variables below:
+release_year      = 2021
+runtime           = 2
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10        = 9.7
+rating_out_of_10_string = str(rating_out_of_10)
+
+print("Cyberpunk: The Movie gets a rating of " + rating_out_of_10_string)
