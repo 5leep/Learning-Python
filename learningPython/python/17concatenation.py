af51ebac0116526c1dfaa231718ec07063c3b3dc
Sleep <git.sleep@gmail.com>
2021-09-03 14:40:36 +0100

    Add files via upload

---
+++ learningPython/python/17concatenation.py
@@ -0,0 +1,16 @@
+string1  = "In the first age, "
+string2  = "In the first battle, "
+string3  = "When the shadows first lengthened. "
+string4  = "One stood. "
+string5  = "He chose the path of perpetual torment. "
+string6  = "In his ravenous hatred he found no peace. "
+string7  = "And with boiling blood he scoured the umbral plains, "
+string8  = "seeking vengeance against the dark lords who had wronged him. "
+string9  = "And those that tasted the bite of his sword named him... "
+string10 = "The Doom Slayer."
+
+# Define message below:
+message = ( string1 + string2 + string3 + string4 + string5 + string6 + string7 + string8 + string9 + string10 )
+
+#print(message)
+print(message)
