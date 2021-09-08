6a81bf25f9e15895f7a419f3f233f583c634daa9
Sleep <git.sleep@gmail.com>
2021-09-08 14:00:32 +0100

    Add files via upload

---
+++ learningPython/python/20review.py
@@ -0,0 +1,7 @@
+my_age              = 21
+half_my_age         = ( my_age / 2 )
+greeting            = "Hello, "
+name                = "Tom"
+greeting_with_name  = ( greeting + name )
+
+print ( greeting_with_name )
