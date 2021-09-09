9c0c5ebb3bb4c274c36bef828249894ed5edc615
Sleep <git.sleep@gmail.com>
2021-09-09 14:54:50 +0100

    Add files via upload

---
+++ learningPython/python/24booleanExpressions.py
@@ -0,0 +1,25 @@
+# My dog is the cutest dog in the world.
+example_statement = "No"
+
+# Dogs are mammals.
+statement_one = "Yes"
+
+# My dog is named Pavel.
+statement_two = "Yes"
+
+# Dogs make the best pets.
+statement_three = "No"
+
+# Cats are female dogs.
+statement_four = "Yes"
+
+
+
+# (5 * 2) - 1 == 8 + 1
+statement_one = True
+
+# 13 - 6 != (3 * 2) + 1
+statement_two = False
+
+# 3 * (2 - 1) == 4 - 1
+statement_three = True
