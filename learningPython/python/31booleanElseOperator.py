f1366b379322c7a1033aedab741d0e72bc950edc
Sleep <git.sleep@gmail.com>
2021-09-12 10:20:06 +0100

    Add files via upload

---
+++ learningPython/python/31booleanElseOperator.py
@@ -0,0 +1,12 @@
+credits = 120
+gpa = 1.9
+
+name = "Tom"
+
+print ('\n\n\n' + 'Hello ' + name + ', the following message regards your college "Grade Point Average" (GPA) and Credit awarded by your efforts.' + '\n')
+
+
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!\n\n")
+else:
+  print("You do not meet the requirements to graduate.\n\n")
