82ec0d7eec5e05f3a9d1c5cbe92fbf42742fbaa0
Sleep <git.sleep@gmail.com>
2021-09-15 17:23:12 +0100

    Add files via upload

---
+++ learningPython/python/38growingAList_+.py
@@ -0,0 +1,11 @@
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# Create new orders here:
+
+new_orders = ["lilac", "iris"]
+
+# broken_prices = [5, 3, 4, 5, 4, 4]
+
+orders_combined = orders + new_orders
+
+print(orders_combined)
