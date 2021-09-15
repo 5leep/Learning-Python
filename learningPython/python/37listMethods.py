f58a9236e81cc110878f1fd8178543c331d5a9c9
Sleep <git.sleep@gmail.com>
2021-09-15 17:23:02 +0100

    Add files via upload

---
+++ learningPython/python/37listMethods.py
@@ -0,0 +1,25 @@
+append_example = [ 'This', 'is', 'an', 'example']
+append_example.append('list')
+ 
+print(append_example)
+
+
+example_list = [1, 2, 3, 4]
+
+#Using Append
+example_list.append(5)
+print(example_list)
+
+#Using Remove
+example_list.remove(5)
+print(example_list)
+
+
+orders = ["daisies", "periwinkle"]
+
+print(orders)
+
+orders.append("tulips")
+orders.append("roses")
+
+print(orders)
