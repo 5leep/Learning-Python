ec65547a59c315633ba4072874ab397212da6c8c
Sleep <git.sleep@gmail.com>
2021-09-20 20:03:47 +0100

    Add files via upload

---
+++ learningPython/python/63whileLoops.py
@@ -0,0 +1,23 @@
+# While Loop Walkthrough
+count = 0
+print("Starting While Loop 1")
+while count <= 3:
+  # Loop Body
+  # Print if the condition is still true
+  print("Loop Iteration - count <= 3 is still true")
+  # Print the current value of count 
+  print("Count is currently " + str(count))
+  # Increment count
+  count += 1
+  print(" ----- ")
+print("While Loop ended\n\n\n")
+
+# Your code below: 
+count2 = 10
+print("Starting While Loop 2")
+while count2 <= 10:
+  print("Loop iteration START...")
+  print("Loop iteration is currently registering at: " + str(count))
+  count2 -= 1
+  print(" ----- ")
+print("While loop END.")
