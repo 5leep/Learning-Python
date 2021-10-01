6b57e7591be38ef71fd7bf2ebb45a82a4f050c27
Sleep <git.sleep@gmail.com>
2021-10-01 14:12:40 +0100

    Add files via upload

---
+++ learningPython/python/PythonChatBots/Echo.Chatbot.py
@@ -0,0 +1,4 @@
+print("\nHello. I am echo, your personal chatbot.")
+print("\nI will repeat after you")
+stuff_to_echo = input("\nEnter something for me to echo:\n")
+print("\nYou said: " + '"' + stuff_to_echo + '"')
