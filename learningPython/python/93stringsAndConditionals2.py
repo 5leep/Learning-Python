2036056304464bd898f5c3586ebc552beac777b6
Sleep <git.sleep@gmail.com>
2021-10-05 10:48:19 +0100

    Add files via upload

---
+++ learningPython/python/93stringsAndConditionals2.py
@@ -0,0 +1,15 @@
+def contains(big_string, little_string):
+  return little_string in big_string
+
+print(contains("watermelon", "melon"))
+
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
+
+print(common_letters("banana", "cream"))
+print(common_letters("Abracadabra", "Abracadabra"))
