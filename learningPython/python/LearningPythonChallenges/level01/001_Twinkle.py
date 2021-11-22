e474253298edb95bfe90e9f27b0e44509b0d7c4c
Sleep <git.sleep@gmail.com>
2021-11-22 11:47:58 +0000

    Add files via upload

---
+++ learningPython/python/LearningPythonChallenges/level01/001_Twinkle.py
@@ -0,0 +1,19 @@
+# My solution...
+
+twinkle = """
+Twinkle, twinkle, little star,
+    How I wonder how you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+    How I wonder what you are
+"""
+
+print(twinkle)
+# It works!
+
+
+# Example solution...
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a "
+      "diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
