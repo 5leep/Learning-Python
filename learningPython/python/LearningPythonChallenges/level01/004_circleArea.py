04cef800ae091a29c61e3035d50a371865642043
Sleep <git.sleep@gmail.com>
2021-11-22 11:48:55 +0000

    Add files via upload

---
+++ learningPython/python/LearningPythonChallenges/level01/004_circleArea.py
@@ -0,0 +1,29 @@
+# My solution...
+
+# A = area
+# π = pi
+# r = radius
+
+# Area of a circle = A = πr**2
+
+import math
+
+def getMeThatArea(r):
+    area = (math.pi) * r**2
+    return area
+
+print ("\nThe area of your circle is " + str(getMeThatArea(1.1)))
+
+# It works!
+
+
+# Example solution
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# My amended solution...
+
+# Things go here...
