8031bdf1b890e6f7842bd8ce86f19fc498b8c2c9
Sleep <git.sleep@gmail.com>
2021-11-22 11:48:22 +0000

    Add files via upload

---
+++ learningPython/python/LearningPythonChallenges/level01/002_PyVersion.py
@@ -0,0 +1,22 @@
+# My solution
+
+import sys
+
+print(sys.version)
+
+# It works!
+
+# Example solution...
+
+# import sys
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
+
+# My amended solution...
+
+# import sys
+
+print("Python version:\n" + str(sys.version) + "\nVersion info:\n" + str(sys.version_info))
