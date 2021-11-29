adabeecd1ade4985adcc4e5b0dffb5c0f328c358
Sleep <git.sleep@gmail.com>
2021-11-29 11:52:36 +0000

    Add files via upload

---
+++ learningPython/python/LearningPythonChallenges/level01/008_FirstAndLast.py
@@ -0,0 +1,13 @@
+# Write a Python program to display the first and last colors from the following list.
+
+# My solution...
+
+colour_list = ["Red","Green","White" ,"Black"]
+print("The colours you require are:\n'" + str(colour_list[0]) + "' and '" + str(colour_list[-1]) + "'.")
+
+# It works?
+
+# Their solution...
+
+color_list = ["Red","Green","White" ,"Black"]
+print( "%s %s"%(color_list[0],color_list[-1]))
