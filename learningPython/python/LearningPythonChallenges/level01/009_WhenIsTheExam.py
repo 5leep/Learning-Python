8481ba4918c3eb4410a06eca99a62fff9a366bcc
Sleep <git.sleep@gmail.com>
2021-11-29 11:52:51 +0000

    Add files via upload

---
+++ learningPython/python/LearningPythonChallenges/level01/009_WhenIsTheExam.py
@@ -0,0 +1,19 @@
+# Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+# My solution...
+
+exam_st_date = (11, 12, 2014)
+
+print("The exam is " + str(exam_st_date[0]) + " / " + str(exam_st_date[1]) + " / " + str(exam_st_date[2]))
+
+# It works!
+
+
+# Their Solution...
+
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
+
+# My amended solution...
+
